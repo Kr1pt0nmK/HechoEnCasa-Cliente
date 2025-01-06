4d117f47e64d65bb424378422433f1c10cf78e25
Christian <crissmares07@gmail.com>
2025-01-06 02:01:04 -0600

    Todos los ingredientes se muestran de forma correcta

--- PHP/inventario.php
+++ PHP/inventario.php
@@ -9,9 +9,9 @@ if ($accion == "mostrar") {
     $query = "SELECT nombre, stock, unidad_total FROM ingrediente";
     $rs = mysqli_query($con, $query);
 
-    $datos = [];
+    $datos = array();
     while ($tupla = mysqli_fetch_assoc($rs)) {
-        $datos = $tupla;
+        $datos[] = $tupla;
     }
 
     echo json_encode($datos);
