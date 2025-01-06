377e44347d961cf6f8929feea99fef357253142a
Christian <crissmares07@gmail.com>
2025-01-06 01:40:21 -0600

    Prueba para mostrar todos los ingredientes con PHP

---
+++ PHP/inventario.php
@@ -0,0 +1,18 @@
+<?php
+
+include "conexion.php";
+
+$accion = $_GET['action'] ?? '';
+
+if ($accion == "mostrar") {
+
+    $query = "SELECT nombre, stock, unidad_total FROM ingrediente";
+    $rs = mysqli_query($con, $query);
+
+    $datos = [];
+    while ($tupla = mysqli_fetch_assoc($rs)) {
+        $datos = $tupla;
+    }
+
+    echo json_encode($datos);
+}
