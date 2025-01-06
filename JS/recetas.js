1c0d8e67d78412abca8ad51b670f6d6642b01b67
Christian <crissmares07@gmail.com>
2025-01-06 02:17:47 -0600

    Prueba exitosa de backend con las recetas

---
+++ JS/recetas.js
@@ -0,0 +1,30 @@
+const contElementos = document.querySelector(".contenedor-elementos")
+
+document.addEventListener("DOMContentLoaded", () => {
+    mostrarIngredientes();
+})
+
+let mostrarIngredientes = () => {
+
+    fetch("../PHP/recetas.php?action=mostrar")
+    .then(Response => Response.json())
+    .then(elemento => {
+        elemento.forEach(tupla => {
+            let cajaElemento = document.createElement("div")
+            cajaElemento.className = "caja-elemento"
+
+            cajaElemento.innerHTML = `
+                <div class="color-header"></div>
+                <div class="nombre-receta"><h2>${tupla.nombre}</h2></div>
+                <hr />
+                <div class="porciones-cantidad">
+                    <p class="cantidad">${tupla.num_porciones}</p>
+                    <p class="unidad">porciones</p>
+                </div>
+            `
+            contElementos.appendChild(cajaElemento)
+        })
+    })
+    .catch(error => console.error("Error al cargar los datos:", error));
+
+}
---
+++ PHP/recetas.php
@@ -0,0 +1,18 @@
+<?php
+
+include "conexion.php";
+
+$accion = $_GET['action'] ?? '';
+
+if ($accion == "mostrar") {
+
+    $query = "SELECT nombre, num_porciones FROM receta_base";
+    $rs = mysqli_query($con, $query);
+
+    $datos = array();
+    while ($tupla = mysqli_fetch_assoc($rs)) {
+        $datos[] = $tupla;
+    }
+
+    echo json_encode($datos);
+}
